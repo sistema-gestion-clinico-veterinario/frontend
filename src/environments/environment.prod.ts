8f20286d15122152b7ad6c758ec706c3bf8a8f11
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-06-13 07:19:07 -0500

    chore: actualizar apiUrl a produccion en environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://backend-cwx3.onrender.com/api/v1',
   mpPublicKey: 'APP_USR-REEMPLAZAR-CON-TU-PUBLIC-KEY-PRODUCCION'
 };
