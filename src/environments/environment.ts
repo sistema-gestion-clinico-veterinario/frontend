8e4c3c2f3a6e94239493d78456d79e184cb9c192
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-06-28 15:57:57 -0500

    Cambios de enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-    //apiUrl: 'https://backend-cwx3.onrender.com/api/v1',
-   apiUrl: 'http://localhost:8080/api/v1',
+    apiUrl: 'https://backend-cwx3.onrender.com/api/v1',
+   //apiUrl: 'http://localhost:8080/api/v1',
   iaUrl: 'https://modelos-production.up.railway.app',
 };
