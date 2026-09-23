712d4400b7852da3c9e083d5fd4ae4632cb726bd
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-09-23 00:35:10 -0500

    fix(auth): evitar sesión cruzada entre empresas en la misma pestaña/navegador y eliminar login sin slug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true,
   apiUrl: '/api/v1',
   wsApiUrl: 'https://backend-cwx3.onrender.com/api/v1',
-  iaUrl: 'https://modelos-production.up.railway.app',
-  mpPublicKey: 'APP_USR-REEMPLAZAR-CON-TU-PUBLIC-KEY-PRODUCCION',
+  iaUrl: 'https://modelos-production-ade8.up.railway.app',
   googleClientId: '750484830871-htm9bb2r3rmunvqs75jm1bc6r8ugdpqf.apps.googleusercontent.com',
   googleRedirectUri: 'https://backend-cwx3.onrender.com/api/v1/auth/google/callback',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: '/api/v1',
   wsApiUrl: 'https://backend-cwx3.onrender.com/api/v1',
   //apiUrl: 'http://localhost:8080/api/v1',
-  iaUrl: 'https://modelos-production.up.railway.app',
+  iaUrl: 'https://modelos-production-ade8.up.railway.app',
   googleClientId: '750484830871-htm9bb2r3rmunvqs75jm1bc6r8ugdpqf.apps.googleusercontent.com',
   googleRedirectUri: 'https://backend-cwx3.onrender.com/api/v1/auth/google/callback',
 };
